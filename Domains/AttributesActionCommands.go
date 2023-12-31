2fcde1f3cb053fa95482d94c5aa7a3669af267cd
jlambert68 <jonas.lambert@drakon.se>
2023-12-31 15:27:21 +0100

    Add +1 to values to be compatible with gRPC-conversions 	AttributeActionCommand_USE_DROPZONE_VALUE_FOR_ATTRIBUTE      TypeAndStructs.AttributeActionCommandType = 1 	AttributeActionCommand_REMOVE_ATTRIBUTE_FROM_TESTINSTRUCTION TypeAndStructs.AttributeActionCommandType = 2

--- Domains/AttributesActionCommands.go
+++ Domains/AttributesActionCommands.go
@@ -4,6 +4,6 @@ import "github.com/jlambert68/FenixTestInstructionsAdminShared/TypeAndStructs"
 
 // ** AttributeActionCommands ***
 const (
-	AttributeActionCommand_USE_DROPZONE_VALUE_FOR_ATTRIBUTE      TypeAndStructs.AttributeActionCommandType = 0
-	AttributeActionCommand_REMOVE_ATTRIBUTE_FROM_TESTINSTRUCTION TypeAndStructs.AttributeActionCommandType = 1
+	AttributeActionCommand_USE_DROPZONE_VALUE_FOR_ATTRIBUTE      TypeAndStructs.AttributeActionCommandType = 1
+	AttributeActionCommand_REMOVE_ATTRIBUTE_FROM_TESTINSTRUCTION TypeAndStructs.AttributeActionCommandType = 2
 )
