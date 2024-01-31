b5b16ba6ecb9bc0a165730acf92700b9cd05ac87
jlambert68 <jonas.lambert@drakon.se>
2024-01-31 11:05:23 +0100

    Changed so the AllowedUsers-file is passed as a []byte for parsing

--- shared_code/importAllowedUserFromFile.go
+++ shared_code/importAllowedUserFromFile.go
@@ -3,25 +3,14 @@ package shared_code
 import (
 	"encoding/json"
 	"log"
-	"os"
 )
 
-func ImportAllowedUsersFromFile() (err error) {
+func ParseAllowedUsersFromFile(allowedUsers []byte) (err error) {
 
-	// Get Environment variable for relative path us json file with allowed users for this connector
-	RelativePathToAllowedUsersList = MustGetenv("RelativePathToAllowedUsersList")
-
-	var allowedUsersFile *os.File
-	allowedUsersFile, err = os.Open(RelativePathToAllowedUsersList)
+	err = json.Unmarshal(allowedUsers, &AllowedUsersLoadFromJsonFile)
 
 	if err != nil {
-		log.Fatalln("opening json file containing allowed users", err.Error())
-		return err
-	}
-
-	jsonParser := json.NewDecoder(allowedUsersFile)
-	if err = jsonParser.Decode(&AllowedUsersLoadFromJsonFile); err != nil {
-		log.Fatalln("parsing json file containing allowed users", err.Error())
+		log.Fatalln("parsing json '[]byte' containing allowed users", err.Error())
 
 		return err
 	}
